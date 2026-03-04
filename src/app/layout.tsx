62fd1b8f7f2fc26c6101e16b8770981d7dce6e8d
djsy01 <dlsgh8884@naver.com>
2026-03-04 22:25:05 +0900

    설명문 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Inho | Portfolio",
-  description: "프론트엔드 개발자 엄인호의 포트폴리오",
+    title: 'Inho | Portfolio',
+    description: '개발자 엄인호의 포트폴리오',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ko">
-      <body suppressHydrationWarning>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="ko">
+            <body suppressHydrationWarning>{children}</body>
+        </html>
+    );
 }
